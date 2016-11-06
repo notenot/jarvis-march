c22830ea2aa3cb94816b90fcbc8222894b3a99c6
notenot <notsu@mail.ru>
2016-11-06 19:48:08 +0300

    Add tests for lib

---
+++ JarvisMarchTests/AlgorithmTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using JarvisMarchLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JarvisMarchTests
+{
+    [TestClass]
+    public class AlgorithmTests
+    {
+        [TestMethod]
+        public void TestCalculate()
+        {
+            var a = new Point(1, 6);
+            var b = new Point(7, 5);
+            var c = new Point(1, 1);
+            var d = new Point(2, 4);
+            var e = new Point(4, 7);
+
+            var points = new LinkedList<Point>(new Point[5] 
+                { a, b, c, d, e }); 
+            var expected = new LinkedList<Point>(new Point[4] 
+                { c, a, e, b });
+            var actual = JarvisMarch.Calculate(points);
+
+            AreEqualLinkedLists(actual, expected);
+
+            a = new Point(6, 6);
+            b = new Point(5, 5);
+            c = new Point(7, 7);
+            d = new Point(3, 1);
+            e = new Point(7, 2);
+            var f = new Point(6, 3);
+            var j = new Point(3, 5);
+
+            points = new LinkedList<Point>(new Point[7] 
+                { a, b, c, d, e, f, j });
+            expected = new LinkedList<Point>(new Point[4] 
+                { d, j, c, e});
+            actual = JarvisMarch.Calculate(points);
+
+            AreEqualLinkedLists(actual, expected);
+
+            a = new Point(-71, -56);
+            b = new Point(-72, -59);
+            c = new Point(-75, -56);
+            d = new Point(-70, -55);
+            e = new Point(-72, -58);
+
+            points = new LinkedList<Point>(new Point[5] 
+                { a, b, c, d, e });
+            expected = new LinkedList<Point>(new Point[3] 
+                { c, d, b });
+            actual = JarvisMarch.Calculate(points);
+
+            AreEqualLinkedLists(actual, expected);
+        }
+
+        private static void AreEqualLinkedLists(LinkedList<Point> expected, 
+            LinkedList<Point> actual)
+        {
+            if (expected.Count != actual.Count)
+                Assert.Fail();
+
+            if (expected.Count == 0)
+                return;
+
+            var nodeExp = expected.First;
+            var nodeAct = actual.First;
+
+            while (nodeExp != null)
+            {
+                Assert.AreEqual(nodeExp.Value, nodeAct.Value);
+                nodeExp = nodeExp.Next;
+                nodeAct = nodeAct.Next;
+            }
+        }
+    }
+}
---
+++ JarvisMarchTests/DirectionTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+using JarvisMarchLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JarvisMarchTests
+{
+    [TestClass]
+    public class DirectionTests
+    {
+        [TestMethod]
+        public void TestCalculate()
+        {
+            var a = new Point(0, 0);
+            var b = new Point(1, 1);
+            var c = new Point(2, 1);
+
+            var actual = Direction.Calculate(a, b, c);
+            Assert.AreEqual(Direction.Value.Right, actual);
+
+            a = new Point(-2, -10);
+            b = new Point(-30, -9);
+            c = new Point(-30, -50);
+
+            actual = Direction.Calculate(a, b, c);
+            Assert.AreEqual(Direction.Value.Left, actual);
+
+            a = new Point(-100, -100);
+            b = new Point(-2, -2);
+            c = new Point(30, 30);
+
+            actual = Direction.Calculate(a, b, c);
+            Assert.AreEqual(Direction.Value.Straight, actual);
+        }
+    }
+}
